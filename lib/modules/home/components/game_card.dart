75467234f41ce6098ead18c4b5fa4af903ac8d64
Lucas Santos <lucas-unifei@outlook.com>
2021-10-19 10:29:19 -0300

    fix: game card
    Fix title in game card, removes windows and web build and add cached
    network image with loading and error image

--- lib/modules/home/components/game_card.dart
+++ lib/modules/home/components/game_card.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:free_games/data/models/game_model.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 
 class GameCard extends StatelessWidget {
   final Game game;
 
-  const GameCard(this.game, {Key? key}) : super(key: key);
+  const GameCard(this.game);
 
   @override
   Widget build(BuildContext context) {
@@ -14,30 +15,58 @@ class GameCard extends StatelessWidget {
       child: InkWell(
         onTap: () {},
         child: Container(
-          height: 400,
-          child: Card(
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(10),
-            ),
-            child: Column(
-              children: [
-                Container(
-                  height: 120,
-                  child: Image.network(
-                    game.thumbnail ?? '',
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(10),
+                topRight: Radius.circular(10),
+                bottomLeft: Radius.circular(10),
+                bottomRight: Radius.circular(10)),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey.withOpacity(0.5),
+                spreadRadius: 5,
+                blurRadius: 7,
+                offset: Offset(0, 3), // changes position of shadow
+              ),
+            ],
+          ),
+          height: 240,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              ClipRRect(
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(10),
+                    topRight: Radius.circular(10),
+                  ),
+                  child: CachedNetworkImage(
+                    imageUrl:
+                        game.thumbnail ?? 'http://via.placeholder.com/350x150',
                     fit: BoxFit.fill,
-                  ),
+                    height: 120,
+                    placeholder: (context, url) => Container(
+                      alignment: Alignment.center,
+                      child: SizedBox(
+                          width: 24,
+                          height: 24,
+                          child: CircularProgressIndicator()),
+                    ),
+                    errorWidget: (context, url, error) => Icon(Icons.error),
+                  )),
+              Container(
+                padding: EdgeInsets.all(8),
+                child: Column(
+                  children: [
+                    Text(
+                      game.title ?? '',
+                      style: textTheme.headline5,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  ],
                 ),
-                SizedBox(height: 10),
-                Center(
-                  child: Text(
-                    game.title ?? '',
-                    style: textTheme.headline5,
-                    textAlign: TextAlign.center,
-                  ),
-                ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
